36d36ec4398c00ce61cb21a2c01db03e87b6a62b
David Spanier <>
2018-01-08 17:08:38 +0100

    Mods

--- index.js
+++ index.js
@@ -5,17 +5,18 @@ const EventEmitter = require('events');
 
 const SerialPort = require('serialport');
 const ESP3Parser = require('esp3-parser');
-const ESPPacket = require('esp3-packet');
-const EEPPacket = require('eep-packet');
+const EEPParser = require('eep-parser');
+// const ESPPacket = require('esp3-packet');
+// const EEPPacket = require('eep-packet');
 
 /*
  *  Private declarations
  */
-const _config = {};
-let _knownDevices;
-let _learnMode = false;
-let _serialport;
-let _parser;
+var _config = null;
+var _knownDevices = null;
+var _learnMode = false;
+var _serialport = null;
+var _parser = null;
 
 /*
  *  Class
@@ -47,11 +48,9 @@ class ESP3 extends EventEmitter {
         _learnMode = false;
     }
 
-    addKnownDevice(device) {
-        if (device && device.senderId && device.eep) {
-            _knownDevices.push({senderId: device.senderId, eep: device.eep}); // Add error handling (senderId exists)
-        } else {
-            throw new TypeError('Device is missing or has an invaild format.');
+    addKnownDevice(senderId, eep) {
+        if (senderId && eep && _knownDevices.find((device) => { return device.senderId === senderId; }) === undefined) {
+            _knownDevices.push({senderId: senderId, eep: eep}); // Add error handling (senderId exists)
         }
     }
 
@@ -60,26 +59,24 @@ class ESP3 extends EventEmitter {
     }
 
     open() {
-        _serialport.open(function(err) {
+        _serialport.open((err) => {
             if (err) {
                 this.emit('esp-error', err);
             }
-        }.bind(this));
+        });
 
-        _parser.on('data', function(buffer) {
-            const eepPacket = new EEPPacket();
-            eepPacket.setParser(new ESPPacket());
-            eepPacket.setKnownDevices(_knownDevices);
+        _parser.on('data', (buf) => {
+            const eepParser = new EEPParser();
+            eepParser.addDevices(_knownDevices);
     
-            const packet = eepPacket.parse(buffer);
+            const packet = eepParser.parse(buf);
 
             if (_learnMode && packet && packet.learnMode) {
                 this.emit('new-device', packet);
             } else if (packet && !packet.learnMode) {
                 this.emit('known-device', packet);
             }
-
-        }.bind(this));
+        });
     }
 
     write() {
